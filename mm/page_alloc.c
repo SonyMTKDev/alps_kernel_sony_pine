57e8f4e2ccb58fc934dc275f256898ad97351d13
jmpfbmx <jmpf.bmx@gmail.com>
2021-10-04 21:04:32 +0200

    mt673x: mm: Import SONY changes
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: Ib9490d9f76bb982e6388a9b8090305746cddc9ac

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1609,19 +1609,6 @@ void split_page(struct page *page, unsigned int order)
 		split_page(virt_to_page(page[0].shadow), order);
 #endif
 
-#ifdef CONFIG_PAGE_OWNER_SLIM
-	/*
-	 * Normal page owner do not need to reset page owner for split
-	 * page, because page_ext belong to each page.
-	 * Normal page owner directly rewrite owner of page_ext.
-	 * In CONFIG_PAGE_OWNER_SLIM case, number of page count will
-	 * break balance (allocate/free) when directly rewrite page
-	 * owner.
-	 * Split page need to modify owner surly. We reset page owner
-	 * than set page owner to guarantee allocate/free are balanced.
-	 */
-	reset_page_owner(page, order);
-#endif
 	set_page_owner(page, 0, 0);
 	for (i = 1; i < (1 << order); i++) {
 		set_page_refcounted(page + i);
@@ -2342,6 +2329,7 @@ void warn_alloc_failed(gfp_t gfp_mask, unsigned int order, const char *fmt, ...)
 	pr_warn("%s: page allocation failure: order:%u, mode:0x%x\n",
 		current->comm, order, gfp_mask);
 
+	trace_mm_page_alloc_fail(order);
 	dump_stack();
 	if (!should_suppress_show_mem())
 		show_mem(filter);
@@ -3072,6 +3060,10 @@ retry_cpuset:
 
 #endif /* __LOG_PAGE_ALLOC_ORDER__ */
 	trace_mm_page_alloc(page, order, gfp_mask, migratetype);
+
+	if (order > 1)
+		trace_mm_page_alloc_highorder(page, order,
+					      gfp_mask, migratetype);
 
 out:
 	/*
@@ -6004,8 +5996,9 @@ static void __setup_per_zone_wmarks(void)
 
 		zone->watermark[WMARK_LOW]  = min_wmark_pages(zone) +
 					low + (min >> 2);
-		zone->watermark[WMARK_HIGH] = min_wmark_pages(zone) +
-					low + (min >> 1);
+		zone->watermark[WMARK_HIGH] = zone->watermark[WMARK_LOW]*3;
+		//zone->watermark[WMARK_HIGH] = min_wmark_pages(zone) +
+		//			low + (min >> 1);
 
 		__mod_zone_page_state(zone, NR_ALLOC_BATCH,
 			high_wmark_pages(zone) - low_wmark_pages(zone) -
